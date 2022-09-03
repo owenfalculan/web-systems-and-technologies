5626fa782577bdee0d58d009adcb123fec705e6e
Owen Patrick Falculan <fowenpatrick@gmail.com>
2022-09-03 21:29:49 +0800

    add vars

---
+++ personal-websites/scripts/index.js
@@ -0,0 +1,19 @@
+const section = window.location.pathname.split("/")[3];
+const students = SECTION_MAPPING[section];
+
+for (const student of students) {
+  $("#table-content").append(`
+    <tr>
+      <td>${student.code}</td>
+      <td>${student.name}</td>
+      <td>
+        <a
+          href="/web-systems-and-technologies/personal-websites/${section}/${student.links.website}"
+          class="btn btn-sm btn-${student.links.website ? "success" : "danger"}"
+        >
+          ${student.links.website ? "Open" : "Not Available"}
+        </a>
+      </td>
+    </tr>
+  `);
+}
