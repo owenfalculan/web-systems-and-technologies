f17a904a55f9d6086ac1d0735cba197592c15096
Ays-Kofi <rmmsangalang@gmail.com>
2022-10-03 20:33:32 +0800

    add ralph matthew website link

--- utils/variables.js
+++ utils/variables.js
@@ -2129,8 +2129,8 @@ const IT_3104 = [
     name: "Sangalang, Ralph Matthew M.",
     activities: {
       website: {
-        link: "",
-        code: "",
+        link: "https://github.com/Ays-Kofi/Ralph-Matthew-M.-Sangalang",
+        code: "https://ays-kofi.github.io/Ralph-Matthew-M.-Sangalang/",
       },
     },
   },
