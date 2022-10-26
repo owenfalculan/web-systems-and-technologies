80c32a0891ed4f7bfd7498a8bee7b7892296fe97
markbirds <markbirds@users.noreply.github.com>
2022-10-26 12:21:45 +0000

    deploy: 9fbe39c7ead3f5a94a6b26d361e289d7e5ba54cf

--- _nuxt/static/1666786848/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fweb-systems-and-technologies\u002F_nuxt\u002Fstatic\u002F1666786848",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fweb-systems-and-technologies\u002F",assetsPath:"\u002Fweb-systems-and-technologies\u002F_nuxt\u002F",cdnURL:a}}}}(null));
