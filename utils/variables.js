c829fd25a75c17797e3a6ee39f7c4dcd72408ed8
Owen Patrick Falculan <fowenpatrick@gmail.com>
2022-11-28 18:31:05 +0800

    update arcega links

--- utils/variables.js
+++ utils/variables.js
@@ -1839,8 +1839,8 @@ const IT_3104 = [
     name: "Arcega, Glenn Emerson P.",
     activities: {
       website: {
-        link: "https://glennarcega.github.io/Glennarcega17/",
-        code: "https://github.com/Glennarcega/Glennarcega17.git",
+        link: "https://glennarcega.github.io/glennarcega/",
+        code: "https://github.com/Glennarcega/glennarcega",
       },
     },
   },
